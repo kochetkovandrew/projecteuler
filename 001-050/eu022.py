f22d6812f110a4ee5f850373f771139ea37b4c46
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2019-11-13 00:47:00 +0100

    Euler 22

---
+++ 001-050/eu022.py
@@ -0,0 +1,19 @@
+import itertools
+
+f = open('p022_names.txt', 'r')
+line = f.readlines()[0]
+names = list(map(lambda x: x.strip('"'), line.split(',')))
+names.sort()
+
+
+def char_values(name):
+    return map(lambda char: ord(char) - ord('A') + 1, name)
+
+
+def char_sum(name):
+    return sum(char_values(name))
+
+
+ordered_sums = zip(map(lambda name: char_sum(name), names), itertools.count(1))
+products = itertools.starmap(lambda i, j: i*j, ordered_sums)
+print(sum(products))
