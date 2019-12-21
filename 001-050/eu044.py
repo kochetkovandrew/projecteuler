c033b534073c07ac4a9aa906a650a29ecce09047
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2019-12-21 18:54:25 +0100

    Euler 44

---
+++ 001-050/eu044.py
@@ -0,0 +1,22 @@
+from lib import PentagonalNumber
+
+min_diff = None
+pentagon = PentagonalNumber()
+
+while min_diff is None or (pentagon.numbers[-1] - pentagon.numbers[-2] < min_diff):
+    pentagon.next_number()
+    for i in range(-2, -len(pentagon.numbers), -1):
+        if pentagon.numbers[i]*2 < pentagon.numbers[-1]:
+            break
+        diff1 = pentagon.numbers[-1] - pentagon.numbers[i]
+        summ1 = pentagon.numbers[-1] + pentagon.numbers[i]
+        diff2 = pentagon.numbers[i]
+        summ2 = pentagon.numbers[-1] + diff1
+        if (min_diff is None or diff1 < min_diff) and pentagon.is_pentagonal(diff1) and pentagon.is_pentagonal(summ1):
+            min_diff = diff1
+        if (min_diff is None or diff2 < min_diff) and pentagon.is_pentagonal(diff2) and pentagon.is_pentagonal(summ2) and pentagon.is_pentagonal(diff1):
+            min_diff = diff2
+        if min_diff is not None and diff1 > min_diff:
+            break
+
+print(min_diff)
--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,4 @@
 from .Prime import Prime
 from .Palindrome import check_pal
 from .TriangleNumber import TriangleNumber
+from .PentagonalNumber import PentagonalNumber
