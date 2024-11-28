8eb13a7f3e777cb37c4d3d258199a48bc372b6a9
Andrew Kochetkov <andrew@netmasterclass.net>
2024-11-28 11:16:36 +0100

    Euler 67,81,82,83

---
+++ 051-100/eu083.py
@@ -0,0 +1,35 @@
+f = open('0083_matrix.txt', 'r')
+lines = f.readlines()
+weights = list(map(lambda line: list(map(lambda num: int(num), line.split(','))), lines))
+rows, cols = (len(weights), len(weights[0]))
+sums = [[0 for i in range(cols)] for j in range(rows)]
+sums[0][0] = weights[0][0]
+
+oldsum = 0
+newsum = sum(map(lambda row: sum(row), sums))
+
+while oldsum != newsum:
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if sums[i][j] > 0:
+                if j < cols-1:
+                    right = sums[i][j] + weights[i][j+1]
+                    if sums[i][j+1] == 0 or sums[i][j+1] > right:
+                        sums[i][j+1] = right
+                if i < rows-1:
+                    down = sums[i][j] + weights[i+1][j]
+                    if sums[i+1][j] == 0 or sums[i+1][j] > down:
+                        sums[i+1][j] = down
+                if i > 0:
+                    up = sums[i][j] + weights[i-1][j]
+                    if sums[i-1][j] == 0 or sums[i-1][j] > up:
+                        sums[i-1][j] = up
+                if j > 0:
+                    left = sums[i][j] + weights[i][j-1]
+                    if sums[i][j-1] == 0 or sums[i][j-1] > left:
+                        sums[i][j-1] = left
+
+    oldsum = newsum
+    newsum = sum(map(lambda row: sum(row), sums))
+
+print(sums[rows-1][cols-1])
